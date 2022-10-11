859ee86ec4d2d24c00facd1c9ac37bfba7df49ce
Aleksandra Krzeszewska <akrzeszewska16@wp.pl>
2022-10-11 09:24:55 +0200

    123

--- cal.js
+++ cal.js
@@ -1,7 +1,10 @@
-function dodaj(){
+function submitFunction() {
+  
     var celsius_degrees=parseInt(document.getElementById("celsius_degrees").value);
-    
     var fahrenheit_degrees=celsius_degrees+32;
     document.getElementById("fahrenheit_degrees").innerHTML=fahrenheit_degrees;
+    var celsius_degrees=parseInt(document.getElementById("celsius_degrees").value);
+    var kelvin_degrees=celsius_degrees+273.15;
+    document.getElementById("kelvin_degrees").innerHTML=kelvin_degrees;
  }
  
