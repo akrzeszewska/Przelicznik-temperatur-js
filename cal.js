7bdd6910f92c70a7d4c2e0f60e31127c4d9fcdd6
Aleksandra Krzeszewska <akrzeszewska16@wp.pl>
2022-10-12 00:28:00 +0200

    function suma test

--- cal.js
+++ cal.js
@@ -7,12 +7,29 @@ function submitFunction() {
     var kelvin_degrees=celsius_degrees+273.15;
     document.getElementById("kelvin_degrees").innerHTML=kelvin_degrees;
  }
- const type_degrees = true;
 
-if (type_degrees) {
-    if (type_degrees === "Celcjusza") {
-        function submitFunction() {
-  
-            var celsius_degrees=parseInt(document.getElementById("celsius_degrees").value);
-            var fahrenheit_degrees=celsius_degrees+32;
-        }}}
+ function suma()
+ {
+    const type_degrees = true;
+    if (type_degrees) {
+        if (type_degrees === "Celcjusza")
+            var a = document.getElementById("value_degrees").value;
+            var b = document.getElementById("b").value;
+             a = parseFloat(a);
+             b = 0;
+             alert(a + b);
+        if (type_degrees === "Fafrenheita")
+            var a = document.getElementById("value_degrees").value;
+            var b = document.getElementById("b").value;
+            a = parseFloat(a);
+            b = 32;
+            alert(a + b);
+        if (type_degrees === "Kelvina")
+            var a = document.getElementById("value_degrees").value;
+            var b = document.getElementById("b").value;
+            a = parseFloat(a);
+            b = 273.15;
+            alert(a + b);
+ } }
+
+
